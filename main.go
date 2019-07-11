f875156eab9314f0de6cf1ba7c3a62d2b42cc71d
Jookeret <s5935512034@phuket.psu.ac.th>
2019-07-11 14:30:49 +0700

    get post basic

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/skybase22/test-go-api/router"
+	"github.com/skybase22/test-api/router"
 )
 
 func main() {
 	stringHello := "Hello Jook."
 	fmt.Println(stringHello)
-	router.New(8000).Start()
+	router.New()
+
 }
--- router/router.go
+++ router/router.go
@@ -1,10 +1,12 @@
 package router
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	//"strconv"
 )
 
 type Server struct {
@@ -17,7 +19,14 @@ type Router struct {
 	router *mux.Router
 }
 
-func New(port int) *Server {
+type addressBook struct {
+	Firstname string `json:"first_name"`
+	Lastname  string `json:"last_name"`
+	Code      int    `json:"code,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+}
+
+func New() {
 	router := mux.NewRouter()
 
 	api := router.PathPrefix("/api").Subrouter().StrictSlash(true)
@@ -25,22 +34,50 @@ func New(port int) *Server {
 
 	guest := version.PathPrefix("/guest").Subrouter().StrictSlash(true)
 
-	guest.HandleFunc("/register", getData()).Methods("GET")
+	guest.HandleFunc("/home", getData()).Methods("GET")
+	guest.HandleFunc("/name", sendData()).Methods("POST")
 
-	return &Server{router, port}
+	if err := http.ListenAndServe(":8000", router); err != nil {
+		logrus.Panic("http.ListenAndServe: ", err)
+	}
+
+	//return &Server{router, port}
 
 }
 
 func getData() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to the HomePage!")
+		addNew := &addressBook{
+			Firstname: "Nop",
+			Lastname:  "San",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "max-age=0")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(addNew)
 	}
 }
 
-func (s *Server) Start() {
-	portString := fmt.Sprintf(":%d", s.port)
-	logrus.Infof("Listening on port %s", portString)
-	if err := http.ListenAndServe(portString, s.router); err != nil {
-		logrus.Panic("http.ListenAndServe: ", err)
+func sendData() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		addBook := &addressBook{}
+		err := json.NewDecoder(r.Body).Decode(addBook)
+		if err != nil {
+			return
+		}
+		fmt.Println(addBook)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "max-age=0")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(addBook)
 	}
 }
+
+// func (s *Server) Start() {
+// 	portString := fmt.Sprintf(":%d", s.port)
+// 	logrus.Infof("Listening on port %s", portString)
+// 	fmt.Println(s.port, s.router)
+// 	if err := http.ListenAndServe(portString, s.router); err != nil {
+// 		logrus.Panic("http.ListenAndServe: ", err)
+// 	}
+// }
